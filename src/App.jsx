990da80e03ce79b36b9bd1dbfe1f62aaf819770c
Peter Goddard <petergoddard03@gmail.com>
2026-04-16 13:52:52 -0400

    Changed functionality to have a sliding difficulty

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,30 @@ import './App.css'
 const RandomImageButtons = () => {
   const [pngs, setPNGs] = useState([])
   const [count, setCounter] = useState(0)
-  const [highscore, setHighscore] = useState(0)
+  const [highscore, setHighscore] = useState(1)
   const [count_list, setCountList] = useState([])
+  const [difficulty, setDifficulty] = useState(1)
   var pokemon = ['ditto','pikachu','bulbasaur','charizard','squirtle','weedle','rattata','eevee']
+  var pokemon_parsed = pokemon.slice(1,difficulty+3+1)
   
 
   const randomize = (name) => {
-    
-    console.log(name)
-    console.log(count_list)
-    console.log("randomize!")
-
-    var array = structuredClone(pngs)
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-
+    console.log(count,difficulty+3)
     if (count_list.includes(name)){
 
-      if (highscore < count){
-        setHighscore(count)
+      if (highscore < difficulty){
+        setHighscore(difficulty)
       }
 
+      setCountList([])
+      setCounter(0)
+      setDifficulty(1)
+
+    } else if (count == difficulty + 3 - 1){
+
+      setDifficulty(difficulty+1)
+
+      pokemon_parsed = pokemon.slice(1,difficulty+1)
       setCountList([])
       setCounter(0)
 
@@ -37,6 +38,12 @@ const RandomImageButtons = () => {
       setCounter(count + 1)
     }
 
+    var array = structuredClone(pngs)
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
     setPNGs(array)
 
   };
@@ -45,7 +52,7 @@ const RandomImageButtons = () => {
 
     const fetchData = async () => {
       try {
-        const responses = await Promise.all(pokemon.map(p => fetch('https://pokeapi.co/api/v2/pokemon/'+p)))
+        const responses = await Promise.all(pokemon_parsed.map(p => fetch('https://pokeapi.co/api/v2/pokemon/'+p)))
         const data = await Promise.all(responses.map(response => response.json()))
         setPNGs(data);
         console.log(data)
@@ -57,7 +64,7 @@ const RandomImageButtons = () => {
     fetchData();
     
   
-  }, []);
+  }, [difficulty]);
   return (
     <div> 
       <div id="cards">
@@ -69,9 +76,12 @@ const RandomImageButtons = () => {
           ))}
       </div>
       <div key="3" className="counter">
-          Score: {count}
+          Count: {count}
       </div>
       <div key="4" className="counter">
+          Difficulty: {difficulty}
+      </div>
+      <div key="5" className="counter">
           High Score: {highscore}
       </div>
     </div>
